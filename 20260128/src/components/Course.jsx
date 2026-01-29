0b6abf1d424a0b83a6f4149eaae988e71d880232
MeherNaaz19 <2303a51902@sru.edu.in>
2026-01-29 09:15:32 +0530

    Fix: convert 20260128 from submodule to normal folder

---
+++ 20260128/src/components/Course.jsx
@@ -0,0 +1,11 @@
+function Course() {
+  return (
+    <div>
+      <h3>Course Details</h3>
+      <p>Course: React Basics</p>
+      <p>Duration: 4 Weeks</p>
+    </div>
+  );
+}
+
+export default Course;
